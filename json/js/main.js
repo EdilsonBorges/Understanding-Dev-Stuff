d89d740787114f3424e2882e675347b7a4a17a50
Edilson Borges <edilsonfb@gmail.com>
2017-03-23 00:45:57 -0300

    Using XMLHttpRequest

--- json/js/main.js
+++ json/js/main.js
@@ -1,15 +1,18 @@
-/* var myCat = {
+/*
+// (1)
+var myCat = {
     "name" : "Meowsalot",
     "species" : "cat",
     "favFood" : "tuna"
 }
 myCat.favFood; // accessing data from objects
 
+// (2)
 var myFavColors = ["blue","green","purple"];
-
 myFavColors[1] // accessing data from array
-*/
 
+
+// (3)
 var thePets = [
     {
     "name" : "Meowsalot",
@@ -23,4 +26,12 @@ var thePets = [
     }
 ] // single variable contains an array of two objects, which is a JSON (Java Script Object Notation)
 
-thePets[1].favFood
+thePets[1].favFood;
+*/
+
+var ourRequest = new XMLHttpRequest();
+ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-1.json');
+ourRequest.onload = function() {
+    var ourData = ourRequest.responseText;
+};
+ourRequest.send();
