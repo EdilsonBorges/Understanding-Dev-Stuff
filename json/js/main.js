15863cf311eb2fbc64b301df42445e9bbdcc5f0f
Edilson Borges <edilsonfb@gmail.com>
2017-03-23 07:18:35 -0300

    Getting a bit more complexity - working with AJAX - rendering our HTML dynamically by using functions and web api tools, such as insertAdjacentElement - managing our JSON / Array value - getting status, resolving errors

--- json/js/main.js
+++ json/js/main.js
@@ -1,3 +1,55 @@
+
+
+var pageCounter = 1;
+var animalContaniner = document.getElementById('animal-info');
+var btn = document.getElementById('btn');
+btn.addEventListener('click', function(){
+var ourRequest = new XMLHttpRequest();
+    ourRequest.open('GET', `https://learnwebcode.github.io/json-example/animals-${pageCounter}.json`);
+    ourRequest.onload = function() {
+        if (ourRequest.status >= 200 && ourRequest.status < 400) {
+            var ourData = JSON.parse(ourRequest.responseText);
+            renderHTML(ourData);
+        } else {
+            console.log('we connected to the server, but it returned na error')
+        }
+    };
+
+    ourRequest.onerror = function() {
+        console.log('connection error');
+    }
+
+    ourRequest.send(); 
+    pageCounter++;
+    if(pageCounter > 3) {
+        btn.classList.add('hide-me');
+    }
+});
+
+function renderHTML(data) {
+    var htmlString = '';
+    for(i = 0; i < data.length; i++) {
+        htmlString += `<p> ${data[i].name} is a ${data[i].species} that likes `; // using ES6 template string
+        for (j = 0; j < data[i].foods.likes.length; j++) {
+            if(j == 0) {
+                htmlString += data[i].foods.likes[j];
+            } else {
+                htmlString += ' and ' + data[i].foods.likes[j];
+            }
+        }
+        htmlString += ' and dislikes ';
+        for (j = 0; j < data[i].foods.dislikes.length; j++) {
+            if(j == 0) {
+                htmlString += data[i].foods.dislikes[j];
+            } else {
+                htmlString += ' and ' + data[i].foods.dislikes[j];
+            }
+        }    
+        htmlString += '.</p>';
+    }
+    animalContaniner.insertAdjacentHTML('beforeend', htmlString);
+}
+
 /*
 // (1)
 var myCat = {
@@ -28,10 +80,11 @@ var thePets = [
 
 thePets[1].favFood;
 */
+// AJAX (Asynchronous JavaScript And XML)
+// JSON has kind of replace XML as a format data in many places
 
-var ourRequest = new XMLHttpRequest();
-ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-1.json');
-ourRequest.onload = function() {
-    var ourData = ourRequest.responseText;
-};
-ourRequest.send();
+// (4)
+// http://www.filltext.com/
+
+// (5)
+// handlebars.js
