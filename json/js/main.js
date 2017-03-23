9345c6b65ec0d7c5a4250926480edf451e16487c
Edilson Borges <edilsonfb@gmail.com>
2017-03-23 00:27:03 -0300

    Undersanting JSON

---
+++ json/js/main.js
@@ -0,0 +1,26 @@
+/* var myCat = {
+    "name" : "Meowsalot",
+    "species" : "cat",
+    "favFood" : "tuna"
+}
+myCat.favFood; // accessing data from objects
+
+var myFavColors = ["blue","green","purple"];
+
+myFavColors[1] // accessing data from array
+*/
+
+var thePets = [
+    {
+    "name" : "Meowsalot",
+    "species" : "cat",
+    "favFood" : "tuna"
+    },
+    {
+    "name" : "Barky",
+    "species" : "dog",
+    "favFood" : "carrots"
+    }
+] // single variable contains an array of two objects, which is a JSON (Java Script Object Notation)
+
+thePets[1].favFood
